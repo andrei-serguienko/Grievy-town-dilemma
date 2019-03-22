d4a53e63b7663847b122b670b16356d935817ddb
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-03-22 17:08:55 +0000

    EnterScene done

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,26 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class PlayerManager : MonoBehaviour {
     //public GameObject inventory;
     //public GameObject shop;
     public Image[] Hearts;
+    public bool hasTriggerIntroCinematic = false;
+    public GameObject introTimeline;
 
     // Use this for initialization
-    void Awake()
-    {
+  void Awake()
+  {
         //shop = GameObject.FindWithTag("Shop");
         //shop.SetActive(false);
         //inventory = GameObject.FindWithTag("Inventory");
         //inventory.SetActive(false);
-    }
-    void Start () {
-        
-    }
-	
+  }
+  void Start () {
+  }
+
+
 	// Update is called once per frame
 	void Update () {
+      print(hasTriggerIntroCinematic);
+      if(SceneManager.GetActiveScene().name == "Town" && !hasTriggerIntroCinematic)
+      {
+        introTimeline = GameObject.FindWithTag("TimeLineManager");
+        introTimeline.GetComponent<PlayableDirector>().Play();
+        hasTriggerIntroCinematic = true;
+      }
+
         //if (Input.GetKeyDown(KeyCode.I) && !inventory.activeSelf)
         //{
         //    inventory.SetActive(true);
