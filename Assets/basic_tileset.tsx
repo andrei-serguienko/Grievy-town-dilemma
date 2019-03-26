edff04ec5f0d795964d4179cec13017b2a47e795
Maxime Garcia <maxime.garcia@etu.univ-amu.fr>
2019-03-26 16:46:41 +0000

    SuperTiled2Unity imported

---
+++ Assets/basic_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="3477" columns="57">
+ <image source="./52347556_553512321719081_6728289276770910208_n.png" width="968" height="1052"/>
+</tileset>
