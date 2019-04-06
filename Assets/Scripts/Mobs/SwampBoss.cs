02146599ed2d62f0a7f325a17e369a4c1e99b776
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-04-06 20:43:05 +0100

    4 ulti

--- Assets/Scripts/Mobs/SwampBoss.cs
+++ Assets/Scripts/Mobs/SwampBoss.cs
@@ -18,18 +18,29 @@ public class SwampBoss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      print(hp.LifePoints);
-      print(hp.maxHealth);
-      print(0.8 *(float)hp.maxHealth);
-      print((float)hp.LifePoints < (0.8 * (float)hp.maxHealth));
       if(hp.LifePoints < (0.8 * (float)hp.maxHealth) && !ulti1){
           print("Ulti1");
           GetComponent<Animator>().SetTrigger("Ulti");
           ulti1 = true;
       }
+      if(hp.LifePoints < (0.6 * (float)hp.maxHealth) && !ulti2){
+          print("Ulti1");
+          GetComponent<Animator>().SetTrigger("Ulti");
+          ulti2 = true;
+      }
+      if(hp.LifePoints < (0.4 * (float)hp.maxHealth) && !ulti3){
+          print("Ulti1");
+          GetComponent<Animator>().SetTrigger("Ulti");
+          ulti3 = true;
+      }
+      if(hp.LifePoints < (0.2 * (float)hp.maxHealth) && !ulti4){
+          print("Ulti1");
+          GetComponent<Animator>().SetTrigger("Ulti");
+          ulti4 = true;
+      }
     }
 
-  
+
 
 
 }
