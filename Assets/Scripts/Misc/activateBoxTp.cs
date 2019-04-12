a84b7a626f8f6c0b9c7a531712a5243c4794b61b
Maxime Garcia <maxime.garcia@etu.univ-amu.fr>
2019-04-12 16:20:40 +0100

    Box tps activation working

---
+++ Assets/Scripts/Misc/activateBoxTp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateBoxTp : MonoBehaviour
+{
+    public GameObject Player;
+    public int stepActivation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        Player = GameObject.FindGameObjectWithTag("Player");
+        if (Player.GetComponent<PlayerController>().houxStep >= stepActivation)
+            gameObject.SetActive(true);
+    }   
+}
